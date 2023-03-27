4f32d535ecb55cc8147336eb58fafa8fe4ea8082
PITCHAYAKIT-LEG\Pitchayakit <pitchayakit.p@gmail.com>
2023-03-27 09:11:35 +0700

    Add goodday root folder id in env

--- config/goodday.php
+++ config/goodday.php
@@ -3,6 +3,7 @@
 return [
     'api_token' => env('GOODDAY_API_KEY', null),
     'project_template_id' => env('GOODDAY_PROJECT_TEMPLATE_ID', null),
+    'goodday_root_folder_id' => env('GOODDAY_ROOT_FOLDER_ID', null),
 ];
 
 
